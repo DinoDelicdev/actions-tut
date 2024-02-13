4e19ce0209394eec3e92cff1322a32f2f5da3b14
Dino Delic <dino.delic.dev@gmail.com>
2024-02-13 10:02:49 +0100

    Trying to run the script

---
+++ read_files.py
@@ -0,0 +1,14 @@
+from github import Github
+
+
+g = Github(os.environ['GH_ACCESS_TOKEN'])
+
+# Replace 'your_username' and 'your_repository' with your GitHub username and repository name
+repo = g.get_repo("DinoDelicdev/actions-tut")
+
+# Iterate over all commits in the repository
+for commit in repo.get_commits():
+    # Iterate over all files changed in each commit
+    for file in commit.files:
+        # Print the filename
+        print(file.filename)
